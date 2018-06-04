0a5ecb21ba5d72f124f6535367dfb61dadaae7d6
Goran Minov <goran.s.minov@gmail.com>
2018-06-04 17:19:29 +0100

    refactor: Created profile menu

--- app/src/main/java/com/lineargs/chatservice/ui/ProfileActivity.java
+++ app/src/main/java/com/lineargs/chatservice/ui/ProfileActivity.java
@@ -1,12 +1,11 @@
 package com.lineargs.chatservice.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lineargs.chatservice.R;
 
@@ -32,6 +31,23 @@ public class ProfileActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                //TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
