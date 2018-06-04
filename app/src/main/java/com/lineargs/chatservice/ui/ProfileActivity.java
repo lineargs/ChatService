406501bb242bfcf4a234fb1814d3b554a5dc7399
Goran Minov <goran.s.minov@gmail.com>
2018-06-04 17:31:54 +0100

    feat: Username and email shown on profile

--- app/src/main/java/com/lineargs/chatservice/ui/ProfileActivity.java
+++ app/src/main/java/com/lineargs/chatservice/ui/ProfileActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lineargs.chatservice.R;
 
 import butterknife.BindView;
@@ -16,18 +19,27 @@ public class ProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.profile_email)
+    TextView profileEmail;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
         setupActionBar();
+        profileEmail.setText(firebaseUser.getEmail());
     }
 
     private void setupActionBar() {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
+        setTitle(firebaseUser.getDisplayName());
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
