a1913b6d0813187eff125494156d95e998e47cf9
Goran Minov <goran.s.minov@gmail.com>
2018-06-04 17:10:54 +0100

    feat: Started implementing Profile Activity

--- app/src/main/java/com/lineargs/chatservice/ui/BaseDrawerActivity.java
+++ app/src/main/java/com/lineargs/chatservice/ui/BaseDrawerActivity.java
@@ -69,6 +69,9 @@ public abstract class BaseDrawerActivity extends BaseActivity {
                 }
                 intent = new Intent(this, MainActivity.class);
                 break;
+            case R.id.nav_profile:
+                intent = new Intent(this, ProfileActivity.class);
+                break;
             case R.id.nav_sign_out:
                 //Sign out
                 AuthUI.getInstance().signOut(this);
---
+++ app/src/main/java/com/lineargs/chatservice/ui/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.lineargs.chatservice.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.lineargs.chatservice.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
