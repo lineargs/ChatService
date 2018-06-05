5e4099f470f8a26f0fc67d17ca8e2de9c3525a61
Goran Minov <goran.s.minov@gmail.com>
2018-06-05 20:19:29 +0100

    feat: Created Profile Edit activity

--- app/src/main/java/com/lineargs/chatservice/ui/ProfileEditActivity.java
+++ app/src/main/java/com/lineargs/chatservice/ui/ProfileEditActivity.java
@@ -1,80 +1,54 @@
 package com.lineargs.chatservice.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.lineargs.chatservice.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileEditActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @BindView(R.id.profile_email)
-    TextView profileEmail;
-    @BindView(R.id.profile_image)
-    ImageView profileImage;
-
-    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_edit);
         ButterKnife.bind(this);
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseUser = firebaseAuth.getCurrentUser();
         setupActionBar();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        loadProfileInfo();
     }
 
     private void setupActionBar() {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        setTitle(firebaseUser.getDisplayName());
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    private void loadProfileInfo() {
-        profileEmail.setText(firebaseUser.getEmail());
-        //TODO Load image
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        getMenuInflater().inflate(R.menu.menu_profile_edit, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_edit:
-                startActivity(new Intent(ProfileActivity.this, ProfileEditActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            case R.id.action_save:
+                //TODO Update profile (Display name and Image)
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+
     }
 }
